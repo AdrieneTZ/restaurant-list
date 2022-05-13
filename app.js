3ae830c724a7e409a9b48589aa2fbd9a0dbe2493
AdrieneTZ <tznali17@gmail.com>
2022-05-13 14:59:29 +0800

    refactor: add file, restaurant.json, and render index page with each loop

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const app = express()
 const port = 3000
 
 const exphbs = require('express-handlebars')
+
+const restaurantsData = require('./restaurant.json')
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
 app.get('/', (req,res) => {
-  res.render('index')
+  res.render('index', {restaurantsData: restaurantsData.results})
 })
+
+
 
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
