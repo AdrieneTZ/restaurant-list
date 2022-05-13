82d863a4a87979587334ebc2c9f7716bd8b8074c
AdrieneTZ <tznali17@gmail.com>
2022-05-13 11:59:04 +0800

    style: load static files

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
 app.get('/', (req,res) => {
-  res.send(`This is my project: restaurant list.`)
+  res.render('index')
 })
 
 app.listen(port, () => {
